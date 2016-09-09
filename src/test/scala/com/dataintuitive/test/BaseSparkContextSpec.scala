ac593975b8fda9c5841755cab3644cdf820f42da
Toni Verbeiren <toni.verbeiren@gmail.com>
2016-09-09 11:21:54 +0200

    Small updates

--- src/test/scala/com/dataintuitive/test/BaseSparkContextSpec.scala
+++ src/test/scala/com/dataintuitive/test/BaseSparkContextSpec.scala
@@ -1,22 +1,12 @@
 package com.dataintuitive.test
 
 import org.apache.spark.{SparkConf, SparkContext}
-import org.scalatest.{Matchers, Spec}
 
-/**
-  * @author Thomas Moerman
-  * Shamelesly copied from
-  * https://github.com/tmoerman/vcf-comp/blob/master/src/test/scala/org/tmoerman/test/spark/BaseSparkContextSpec.scala
-  */
 object BaseSparkContextSpec {
 
   lazy val conf = new SparkConf()
     .setAppName("Test")
     .setMaster("local[*]")
-//    .set("spark.kryo.registrator", "org.tmoerman.adam.fx.serialization.AdamFxKryoRegistrator")
-//    .set("spark.kryo.referenceTracking", "true")
-//    .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-
   lazy val sc = new SparkContext(conf)
 
 }
