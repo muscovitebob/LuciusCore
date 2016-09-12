31b7d01b77954f898e4f0edcae553c9873b78219
Toni Verbeiren <toni.verbeiren@gmail.com>
2016-09-12 11:08:00 +0200

    Merged transformations on vectors of different types

--- src/test/scala/com/dataintuitive/luciuscore/TransformationFunctionsTest.scala
+++ src/test/scala/com/dataintuitive/luciuscore/TransformationFunctionsTest.scala
@@ -60,4 +60,31 @@ class TransformationFunctionsTest extends FlatSpec {
     assert(stats2RankVector(tp) === Array(-3.0,1.0,2.0,0.0,0.0))
   }
 
+
+  info("Test the functions for dealing with vectors")
+
+  val indexSignature = new IndexSignature(Array("1", "-3"))
+  val rankVector = signature2OrderedRankVector(indexSignature, 3)
+
+  "An IndexGeneSignature" should "convert to a dense vector of given length" in {
+    assert(rankVector === Array(2.0, 0.0, -1.0))
+  }
+
+  it should "convert to a dense vector of length smaller than the signature" in {
+    assert(signature2OrderedRankVector(indexSignature, 1) === Array(2.0))
+  }
+
+  "An IndexGeneSignature" should "convert to a dense vector of given length for unordered as well" in {
+    assert(signature2UnorderedRankVector(indexSignature, 3) === Array(1.0, 0.0, -1.0))
+  }
+
+  "Nonzero elements" should "return an array of non-zero elements" in {
+    assert(nonZeroElements(rankVector) === Array((1, 2.0), (3, -1.0)))
+  }
+
+  "A rankVector" should "convert to a sparse signature" in {
+    assert(rankVector2IndexSignature(rankVector).signature === Array("1", "-3"))
+  }
+
+
 }
---
+++ src/test/scala/com/dataintuitive/luciuscore/utils/VectorFunctionsTest.scala
@@ -0,0 +1,8 @@
+package com.dataintuitive.luciuscore.utils
+
+/**
+  * Created by toni on 12/09/16.
+  */
+class VectorFunctionsTest {
+
+}
