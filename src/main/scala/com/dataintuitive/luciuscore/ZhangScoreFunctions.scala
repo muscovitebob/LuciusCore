9328654357ac232f0f0557963ba500e782f2d5d0
Boris Shilov <21023619+borisofmuscovy@users.noreply.github.com>
2019-02-19 14:12:28 +0100

    parallel scoring function implementation and test for single query case

--- src/main/scala/com/dataintuitive/luciuscore/ZhangScoreFunctions.scala
+++ src/main/scala/com/dataintuitive/luciuscore/ZhangScoreFunctions.scala
@@ -33,4 +33,19 @@ object ZhangScoreFunctions {
       .sum
   }
 
+  /**
+    * Score an arbitrary number of rank vectors against a database entry.
+    * @param x DbRow
+    * @param queries Rank vector(s)
+    * @return Map, with DbRows' pwid as the key an a list of length(queries) as the value
+    */
+  def queryDbRow(x: DbRow,
+                   queries: Array[Double]*): Map[Option[String], Seq[Option[Double]]] = {
+    x.sampleAnnotations.r match {
+      case Some(r) => Map(x.pwid ->
+        queries.map(query => Option(connectionScore(r, query))))
+      case _       => Map(None -> List(None))
+    }
+  }
+
 }
