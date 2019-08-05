bb8c7217b611516811da7a3e7af00cebedd54b45
Boris <bob@IMB.local>
2019-08-05 14:31:13 +0200

    start ProfileDatabase

--- src/main/scala/com/dataintuitive/luciuscore/ProfileModel.scala
+++ src/main/scala/com/dataintuitive/luciuscore/ProfileModel.scala
@@ -1,14 +1,22 @@
 package com.dataintuitive.luciuscore
 
+import com.dataintuitive.luciuscore.GeneModel.GenesV2
 import com.dataintuitive.luciuscore.Model._
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
 
 object ProfileModel {
   /**
     * Object to deal with RDD[DbRow] AKA databases of transcriptomic profiles, against which connectivity scoring is possible
     */
 
-  class ProfileDatabase(database: RDD[DbRow]) {
+  class ProfileDatabase(spark: SparkSession, database: RDD[DbRow], geneAnnotations: GenesV2) {
+    // check consistency
+    require(database.map(_.sampleAnnotations.t.get.length).filter(_ != geneAnnotations.genes.length).isEmpty)
+    require(database.map(_.sampleAnnotations.p.get.length).filter(_ != geneAnnotations.genes.length).isEmpty)
+    require(database.map(_.sampleAnnotations.r.get.length).filter(_ != geneAnnotations.genes.length).isEmpty)
+
+
 
   }
 }
