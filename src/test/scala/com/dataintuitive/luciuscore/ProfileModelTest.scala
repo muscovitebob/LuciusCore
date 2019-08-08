3932805fc7c1a7087d2cc48177968254427869e7
Boris <bob@IMB.local>
2019-08-08 13:48:59 +0200

    add extra entry to test database, see if 1:n relations work

--- src/test/scala/com/dataintuitive/luciuscore/ProfileModelTest.scala
+++ src/test/scala/com/dataintuitive/luciuscore/ProfileModelTest.scala
@@ -14,21 +14,22 @@ class ProfileModelTest extends FlatSpec with BaseSparkSessionSpec {
     new GeneAnnotationV2("222103_at", GeneType.Landmark, None, None, Some("ATF1"), None, None),
     new GeneAnnotationV2("201453_x_at", GeneType.Landmark, None, None, Some("RHEB"), None, None),
     new GeneAnnotationV2("200059_s_at", GeneType.Landmark, None, None, Some("RHOA"), None, None),
-    new GeneAnnotationV2("200622_x_at", GeneType.Landmark, None, None, Some("CALM3"), None, None))
+    new GeneAnnotationV2("200622_x_at", GeneType.Landmark, None, None, Some("CALM3"), None, None),
+    new GeneAnnotationV2("220673_at", GeneType.Landmark, None, None, Some("ATF1"), None, None))
   )
 
   val row1 = DbRow(Some("GA666"), SampleAnnotations(
     Sample("sample1"),
-    Some(Array(2, 4, 5, 1, 6)),
-    Some(Array(0.5, 0.05, 0.01, 0.2, 1)),
-    Some(Array(2, 3, 4, 1, 5))),
-    compoundAnnotations = CompoundAnnotations(Compound("glyphosate")))
+    Some(Array(2, 4, 5, 1, 6, 7)), // t
+    Some(Array(0.5, 0.05, 0.01, 0.2, 1, 0.02)), // p
+    Some(Array(2, 3, 4, 1, 5, 6))), // r
+    compoundAnnotations = CompoundAnnotations(Compound("aspirin")))
 
   val row2 = DbRow(Some("GA999"), SampleAnnotations(
     Sample("sample2"),
-    Some(Array(4, 7, 9, 1, 3)),
-    Some(Array(0.05, 0.10, 0.02, 0.15, 0.001)),
-    Some(Array(3, 4, 5, 1, 2))),
+    Some(Array(4, 7, 9, 1, 3, 10)),
+    Some(Array(0.05, 0.10, 0.02, 0.15, 0.001, 0.56)),
+    Some(Array(3, 4, 5, 1, 2, 6))),
     compoundAnnotations = CompoundAnnotations(Compound("glyphosate")))
   val database: RDD[DbRow] = spark.sparkContext.parallelize(Array(row1, row2))
 
@@ -41,12 +42,12 @@ class ProfileModelTest extends FlatSpec with BaseSparkSessionSpec {
     assert(newProfiles.State.database
       .map(row => row.sampleAnnotations)
       .flatMap(annots => List(annots.t.get.size, annots.p.get.size, annots.r.get.size))
-      .filter(_ != 4).isEmpty())
+      .filter(_ != 5).isEmpty())
     assert(newProfiles.State.database
       .filter(_.pwid.get == "GA999")
       .map(row => row.sampleAnnotations.r.get)
       .collect.head.toList
-      == List(3.0, 4.0, 1.0, 2.0))
+      == List(3.0, 4.0, 1.0, 2.0, 5.0))
   }
 
   it should "drop gene symbols and a return a new, consistent, profiledatabase" in {
@@ -56,12 +57,12 @@ class ProfileModelTest extends FlatSpec with BaseSparkSessionSpec {
     assert(newProfiles.State.database
       .map(row => row.sampleAnnotations)
       .flatMap(annots => List(annots.t.get.size, annots.p.get.size, annots.r.get.size))
-      .filter(_ != 4).isEmpty())
+      .filter(_ != 5).isEmpty())
     assert(newProfiles.State.database
       .filter(_.pwid.get == "GA999")
       .map(row => row.sampleAnnotations.r.get)
       .collect.head.toList
-      == List(3.0, 4.0, 1.0, 2.0))
+      == List(3.0, 4.0, 1.0, 2.0, 5.0))
   }
 
   it should "keep gene symbols desired" in {
@@ -76,6 +77,10 @@ class ProfileModelTest extends FlatSpec with BaseSparkSessionSpec {
     val newProfiles = profiles.keepProbesets(keeplist)
     assert(newProfiles.State.geneAnnotations.genes.map(_.probesetid).toList == List("200622_x_at"))
     assert(newProfiles.State.geneAnnotations.genes.map(_.symbol.get).toList == List("CALM3"))
+  }
+
+  "retrieveSignificant" should "correctly retrieve only indices with certain significance tresholds" in {
+
   }
 
 }
